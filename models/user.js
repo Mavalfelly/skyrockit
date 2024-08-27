5eca4c25124e0227796597c97ad6e118cad257cd
MaValFelly <matthewfeliciano16@gmail.com>
2024-08-26 20:11:58 -0400

    updtae

--- models/user.js
+++ models/user.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+//application schema
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],//enum is a string that can only be whatever is inside the []
+  },
+});
+
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +33,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embedding the applicationSchema here
 });
 
 const User = mongoose.model('User', userSchema);
